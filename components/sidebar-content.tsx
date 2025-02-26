f8e1ba18ad3b5fa4e0921fc22a59648dc55add4d
Robin Gillessen <robing@VisualReality.onmicrosoft.com>
2025-02-26 14:30:07 +0100

    Clean up unused imports in sidebar-content component

--- components/sidebar-content.tsx
+++ components/sidebar-content.tsx
@@ -6,21 +6,8 @@ import {
   SidebarGroupLabel,
   SidebarGroupContent,
   SidebarMenu,
-  SidebarMenuItem,
-  SidebarProvider,
 } from "@/components/ui/sidebar";
-import { Filter, Plus } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { SidebarSourcesSkeleton } from "@/components/skeleton-loaders/sidebar-sources-skeleton";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import FileUpload from "./file-upload";
 import { LawArticles } from "./law-articles";
 import { Werkwijzes } from "./werkwijzes";
